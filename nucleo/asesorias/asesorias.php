353e8f0641100a0afbfb5b8325a3656d7a688b39
joselupe2002 <joselupe_2002@hotmail.com>
2021-01-25 14:13:27 -0600

    dddd

--- nucleo/asesorias/asesorias.php
+++ nucleo/asesorias/asesorias.php
@@ -271,7 +271,7 @@
 		$pdf->Ln(4);
 		
 		$pdf->eljefe=$dataEmpl[0]["EMPL_JEFEABREVIA"]." ".$dataEmpl[0]["EMPL_JEFED"];
-		$pdf->elpstojefe=$dataEmpl[0]["EMPL_JEFEFIRMAOF"]
+		$pdf->elpstojefe=$dataEmpl[0]["EMPL_JEFEFIRMAOF"];
 
 	
 		$header = array('CONT.', 'NOMBRE DEL ALUMNO', 'GEN.','PROGRAMA EDUCATIVO','TEMA/ASIGNATURA','FECHA','HORA');		
