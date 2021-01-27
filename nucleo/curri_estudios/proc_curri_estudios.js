b1bf8b4ccaa346e2c4a84618c146f053c7a59006
joselupe2002 <joselupe_2002@hotmail.com>
2021-01-27 11:52:31 -0600

    sss

--- nucleo/curri_estudios/proc_curri_estudios.js
+++ nucleo/curri_estudios/proc_curri_estudios.js
@@ -5,7 +5,7 @@ function verAdjEst(modulo,usuario,essuper){
 	table = $("#G_"+modulo).DataTable();
 	if (table.rows('.selected').data().length>0) {
 	
-			previewAdjunto("adjuntos/docDocumentos/"+table.rows('.selected').data()[0]["CURRI_RUTA"]);
+			previewAdjunto(table.rows('.selected').data()[0]["CURRI_RUTA"]);
 	}
 	else {
 		alert ("Debe seleccionar un Mapa Curricular");
