25ac42527fcef1d28bc3090835fb1e7a2242ba5e
joselupe2002 <joselupe_2002@hotmail.com>
2021-01-28 12:33:22 -0600

    sss

--- nucleo/vecompl_cal/liberacion.php
+++ nucleo/vecompl_cal/liberacion.php
@@ -121,9 +121,9 @@
         
         $pdf->SetFont('Montserrat-Medium','',9);
         $pdf->Ln(10);
-        $pdf->Cell(0,0,$dataGen[0]["inst_fechaof"].$fechaof,0,1,'R');
+        $pdf->Cell(0,0,$dataGen[0]["inst_fechaof"]." ".$fechaof,0,1,'R');
         $pdf->Ln(5);
-        $pdf->Cell(0,0,'OFICIO No. '.utf8_decode($dataof[0]["CONT_NUMOFI"]),0,1,'R');
+       // $pdf->Cell(0,0,'OFICIO No. '.utf8_decode($dataof[0]["CONT_NUMOFI"]),0,1,'R');
         $pdf->SetFont('Montserrat-ExtraBold','B',9);
         $pdf->Ln(20);
         $pdf->Cell(0,0,utf8_decode($escolares),0,1,'L');
@@ -137,15 +137,15 @@
         
 
         
-        $pdf->MultiCell(0,8,"El que suscribe : ".utf8_decode($jefediv).", por este medio se permite hacer de su conocimiento ".
-        		"que el estudiante ". utf8_decode($data[0]["MATRICULAD"])." con n�mero de control ".utf8_decode($data[0]["MATRICULA"]).
-        		" de la carrera de ".utf8_decode($data[0]["CARRERA_ALUMD"])." ha cumplido su actividad complementaria con el nivel de desempe�o ".
-        		utf8_decode($data[0]["PROML"])." y un valor num�rico de ".utf8_decode($data[0]["PROM"]).", durante el periodo escolar ".
-        		utf8_decode($data[0]["CICLO"]." ".$data[0]["CICLOD"])." con un valor curricular de ".utf8_decode($data[0]["CREDITOS"])." cr�ditos.".
+        $pdf->MultiCell(0,8,"El que suscribe : ".utf8_decode($data[0]["RESPONSABLED"]).", por este medio se permite hacer de su conocimiento ".
+        		"que el estudiante ". utf8_decode($data[0]["MATRICULAD"]).utf8_decode(" con número de control ").utf8_decode($data[0]["MATRICULA"]).
+        		" de la carrera de ".utf8_decode($data[0]["CARRERA_ALUMD"]).utf8_decode(" ha cumplido su actividad complementaria con el nivel de desempeño ").
+        		utf8_decode($data[0]["PROML"]).utf8_decode(" y un valor numérico de ").utf8_decode($data[0]["PROM"]).", durante el periodo escolar ".
+        		utf8_decode($data[0]["CICLO"]." ".$data[0]["CICLOD"])." con un valor curricular de ".utf8_decode($data[0]["CREDITOS"]).utf8_decode(" créditos.").
         		" Actividad Desarrollada: ".utf8_decode($data[0]["ACTIVIDADD"])." del ".$fechaini." al ".$fechafin,0,'J', false);
         $pdf->Ln(5);
         $pdf->Ln(5);
-        $pdf->MultiCell(0,8,"Se extiende la presente en la ciudad de Santa María de El Oro, Durango a los ".$eldia." d�as del mes de ".$elmes." de ".$elanio,0,'J', false);
+        $pdf->MultiCell(0,8,"Se extiende la presente en la ciudad de Santa María de El Oro, Durango a los ".$eldia." días del mes de ".$elmes." de ".$elanio,0,'J', false);
         $pdf->Ln(5);
        
         
