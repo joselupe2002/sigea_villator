521511fecc0a13aa637a4fbef12607878abb77f7
joselupe2002 <joselupe_2002@hotmail.com>
2021-02-18 17:22:31 -0600

    SSSS

--- nucleo/pa_residencia/pa_residencia.js
+++ nucleo/pa_residencia/pa_residencia.js
@@ -113,7 +113,7 @@ var miciclo="";
 						
 						losdatos=JSON.parse(data); 						
 
-						if ((porcReal>=85)) {
+						if ((porcReal>=80)) {
 														
 							$("#servicio").append("<div class=\"row\" style=\"text-align:left;\">"+
 							"    <div class=\"col-sm-12\"> "+
