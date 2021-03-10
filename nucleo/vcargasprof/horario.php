d632bad7a3dbe227826901e2c50c149a11094d7b
joselupe2002 <joselupe_2002@hotmail.com>
2021-03-09 18:59:31 -0600

    SSSS

--- nucleo/vcargasprof/horario.php
+++ nucleo/vcargasprof/horario.php
@@ -184,7 +184,7 @@
 			{
 			    $entre=false;
 				$miConex = new Conexion();
-				$resultado=$miConex->getConsulta($_SESSION['bd'],"select a.MATERIAD, CONCAT(a.CARRERA,' ',a.PERIODO), ".
+				$resultado=$miConex->getConsulta($_SESSION['bd'],"select a.MATERIAD, a.MATERIA, ".
                                                  "a.SIE AS GRUPO, LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO, (IFNULL(a.HP,0)+IFNULL(a.HT,0)) as HT".
 						                         " from  vedgrupos a where a.BASE IS NULL and a.CICLO='".$_GET["ciclo"]."' and a.PROFESOR='".$_GET["ID"]."'" );				
 				foreach ($resultado as $row) {
