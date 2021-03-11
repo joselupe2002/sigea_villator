1662d8d983734844eee176e2098de9393a780c83
joselupe2002 <joselupe_2002@hotmail.com>
2021-03-11 09:13:26 -0600

    sss

--- nucleo/pa_datosgen/kardex.php
+++ nucleo/pa_datosgen/kardex.php
@@ -440,7 +440,8 @@
 
         $pdf->parseVar('{matapr}',$materiasaprobadas); // convertimos la variable.
         $pdf->parseVar('{promreprobadas}',round($sumacursadas/($cursadas),0)); // Sacamos el promedio con materias reprobadas
-        $pdf->parseVar('{matcursadas}',$cursadas); // convertimos la variable de materias cursadas
+        //$pdf->parseVar('{matcursadas}',$cursadas); // convertimos la variable de materias cursadas
+        $pdf->parseVar('{matcursadas}',$materiasaprobadas); // convertimos la variable de materias cursadas
 
 //=====================================================================================================
 if (count($data2)>0) {
