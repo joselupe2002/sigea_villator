8b12bdd4335820281d3b9083fc72874bda3c368d
joselupe2002 <joselupe_2002@hotmail.com>
2021-03-12 11:03:24 -0600

    SSSS

--- nucleo/tit_tramite/noadeudo.php
+++ nucleo/tit_tramite/noadeudo.php
@@ -135,6 +135,17 @@
 				}
 				return $data;
 			}
+
+			function LoadCiclo($ciclo)
+			{				
+				
+				$miConex = new Conexion();
+				$resultado=$miConex->getConsulta($_SESSION['bd'],"select * from ciclosesc where CICL_CLAVE='".$ciclo."'");				
+				foreach ($resultado as $row) {
+					$data[] = $row;
+				}
+				return $data;
+			}
 			
 			function LoadDatosGen()
 			{
@@ -148,11 +159,27 @@
 			
 			function Header()
 			{
-				$miutil = new UtilUser();
-                $miutil->getEncabezado($this,'V');			
+				//$miutil = new UtilUser();
+                //$miutil->getEncabezado($this,'V');			
                 //Para que cuando se cambie a la otra pagina empiece a la derecha y la stablas no se descuadren
-                $this->SetX(10);
-                $this->Ln(5);	
+               		
+				//$this->SetX(10);
+                //$this->Ln(5);	
+
+				$this->AddFont('Montserrat-Black','B','Montserrat-Black.php');
+				$this->AddFont('Montserrat-Black','','Montserrat-Black.php');
+				$this->AddFont('Montserrat-Medium','B','Montserrat-Medium.php');
+				$this->AddFont('Montserrat-Medium','','Montserrat-Medium.php');
+				$this->AddFont('Montserrat-SemiBold','','Montserrat-SemiBold.php');
+				$this->AddFont('Montserrat-SemiBold','B','Montserrat-SemiBold.php');
+				$this->AddFont('Montserrat-ExtraBold','B','Montserrat-ExtraBold.php');
+				$this->AddFont('Montserrat-ExtraBold','','Montserrat-ExtraBold.php');
+				$this->AddFont('Montserrat-ExtraBold','I','Montserrat-ExtraBold.php');
+				$this->AddFont('Montserrat-ExtraLight','I','Montserrat-ExtraLight.php');
+				$this->AddFont('Montserrat-ExtraLight','','Montserrat-ExtraLight.php');
+
+				
+
 			}
 		
 			function Footer()
@@ -160,9 +187,6 @@
                 
                 $miutil = new UtilUser();            
                 $miutil->getPie($this,'V');
-
-		
-            
              
 			}
 
@@ -177,76 +201,116 @@
 		$pdf->SetMargins(25, 25 , 25);
 		$pdf->SetAutoPageBreak(true,50); 
 		$pdf->AddPage();
-	
-	
+
 		$miutil = new UtilUser();
-        $pstotit=$miutil->getJefe('303');//Nombre del puesto de coordinacion de titulacion 
-
-
+        $pstoesc=$miutil->getJefe('303');//Nombre del puesto de coordinacion de titulacion 
 		$data = $pdf->LoadData();
 		$dataGen = $pdf->LoadDatosGen();
+		$dataCiclo=$pdf->LoadCiclo($data[0]["CICLOTER"]);
 
-		
-		$pdf->SetFont('Montserrat-ExtraBold','B',10);
-		$pdf->Ln(5);
-		$pdf->Cell(0,0,utf8_decode("COMPROBANTE DE NO ADEUDO"),"",0,'C',false);
-		$pdf->Ln(5);
-
-
-		$pdf->SetFont('Montserrat-Medium','',10);
+		$pdf->Ln();
+		$pdf->SetFont('Montserrat-ExtraBold','',22);
+		$pdf->Image('../../imagenes/empresa/logo2.png',26,20,35,35);
+		$pdf->Cell(30,5,"","",0,'C',false);
+		$pdf->MultiCell(140,10,utf8_decode($dataGen[0]["inst_razon"]),0,'R',false);
 	
-		$pdf->Ln(5);
-		$pdf->SetFont('Montserrat-ExtraBold','',10);
-		$pdf->MultiCell(0,5,utf8_decode("EL C. ".$data[0]["PASANTE"]." DE LA CARRERA DE: ".
-		                $data[0]["CARRERAD"]." NO. DE CONTROL ".$data[0]["MATRICULA"]." NO ADEUDA MATERIAL EN: "),0,'J',false);
-		 
-
-		$pdf->Ln(5);
-		$pdf->Cell(66,5,utf8_decode("DEPARTAMENTO:"),"1",0,'L',false);
-		$pdf->Cell(50,5,utf8_decode("NOMBRE ENCARGADO"),"1",0,'L',false);
-		$pdf->Cell(50,5,utf8_decode("FIRMA"),"1",0,'L',false);
-		$pdf->Ln(5);
-
-		$pdf->SetFont('Montserrat-Medium','',10);
-		$pdf->Cell(66,15,utf8_decode("CENTRO DE INFORMACIÓN"),"1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Ln();
-
-		$pdf->SetFont('Montserrat-Medium','',10);
-		$pdf->Cell(66,15,utf8_decode("RECURSOS FINANCIEROS"),"1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Ln();
-
-		$pdf->SetFont('Montserrat-Medium','',10);
-		$pdf->Cell(66,15,utf8_decode("ACTIVIDADES EXTRAESCOLARES"),"1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Ln();
-
-		$pdf->SetFont('Montserrat-Medium','',10);
-		$pdf->Cell(66,15,utf8_decode("LABORATORIO DE COMPUTO"),"1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Ln();
-
-		$pdf->SetFont('Montserrat-Medium','',10);
-		$pdf->Cell(66,15,utf8_decode("LABORATORIO MULTIFUNCIONAL"),"1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Ln();
-   
-		$pdf->SetFont('Montserrat-Medium','',10);
-		$pdf->Cell(66,15,utf8_decode("DESARROLLO ACADÉMICO"),"1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Cell(50,15,"","1",0,'L',false);
-		$pdf->Ln(30);
-		
-		
-		$pdf->Cell(80,3,"FECHA: ___________________         FIRMA: ___________________ ","",1,'L',false);
-
 	
+	
+
+		
+
+		$pdf->setX(75);
+		$pdf->SetFont('Montserrat-ExtraBold','B',13);
+		$pdf->Ln(5);
+		$pdf->Cell(0,0,utf8_decode("CONSTANCIA DE NO ADEUDO"),"",0,'C',false);
+		$pdf->Ln(10);
+
+
+		$pdf->SetFont('Montserrat-Medium','',12);
+	
+		$pdf->MultiCell(0,5,utf8_decode("A través del presente documento se informa que el (la) alumno(a) a ".
+		"continuación descrito(a) no tiene adeudos económicos, materiales o bienes que pertenezcan al Instituto."),0,'J',false);
+
+		$pdf->Ln(5);
+
+		$pdf->SetFont('Montserrat-Medium','',10);
+
+		$pdf->Cell(50,5,utf8_decode("NOMBRE DEL ALUMNO:"),0,0,'L',false);
+		$pdf->Cell(120,5,utf8_decode($data[0]["PASANTE"]),"B",1,'L',false);
+
+
+		$pdf->Cell(40,5,utf8_decode("NO. DEL CONTROL:"),0,0,'L',false);
+		$pdf->Cell(30,5,utf8_decode($data[0]["MATRICULA"]),"B",0,'L',false);
+
+		$pdf->Cell(60,5,utf8_decode("PERIODO DE FIN DE ESTUDIOS:"),0,0,'L',false);
+		$pdf->Cell(40,5,utf8_decode($dataCiclo[0]["CICL_CLAVE"]." ".$dataCiclo[0]["CICL_DESCRIP"]),"B",1,'L',false);
+
+		$pdf->Cell(50,5,utf8_decode("CARRERA:"),0,0,'L',false);
+		$pdf->Cell(120,5,utf8_decode($data[0]["CARRERAD"]),"B",1,'L',false);
+
+
+		$pdf->SetFont('Montserrat-ExtraBold','B',11);
+		$pdf->Ln(5);
+		$pdf->Cell(0,0,utf8_decode("VISTO BUENO DE LOS DEPARTAMENTOS CON LOS QUE NO TIENE ADEUDO:"),"",0,'C',false);
+
+		
+
+		$pdf->Ln(5);
+		$pdf->Cell(85,5,utf8_decode("NOMBRE:"),"1",0,'L',false);	
+		$pdf->Cell(85,5,utf8_decode("FIRMA"),"1",0,'L',false);
+		$pdf->Ln(5);
+
+		$pdf->SetFont('Montserrat-Medium','',10);
+		$pdf->Cell(85,15,utf8_decode("BIBLIOTECA"),"1",0,'L',false);
+		$pdf->Cell(85,15,"","1",0,'L',false);
+		$pdf->Ln();
+
+		$pdf->SetFont('Montserrat-Medium','',10);
+		$pdf->Cell(85,15,utf8_decode("ACTIVIDADES COMPLEMENTARIAS"),"1",0,'L',false);
+		$pdf->Cell(85,15,"","1",0,'L',false);	
+		$pdf->Ln();
+
+
+		$pdf->SetFont('Montserrat-Medium','',10);
+		$pdf->Cell(85,15,utf8_decode("CENTRO DE CÓMPUTO"),"1",0,'L',false);
+		$pdf->Cell(85,15,"","1",0,'L',false);
+		$pdf->Ln();
+
+		$pdf->SetFont('Montserrat-Medium','',10);
+		$pdf->Cell(85,15,utf8_decode("DIVISIÓN DE INGENIERÍAS"),"1",0,'L',false);
+		$pdf->Cell(85,15,"","1",0,'L',false);
+		$pdf->Ln();
+
+		$pdf->SetFont('Montserrat-Medium','',10);
+		$pdf->Cell(85,15,utf8_decode("RECURSOS FINANCIEROS"),"1",0,'L',false);
+		$pdf->Cell(85,15,"","1",0,'L',false);
+		$pdf->Ln();
+
+		$pdf->SetFont('Montserrat-Black','B',10);
+		$pdf->Cell(70,15,utf8_decode("SOLICITO"),"",0,'C',false);
+		$pdf->Cell(30,15,"","",0,'L',false);
+		$pdf->Cell(70,15,utf8_decode("AUTORIZÓ"),"",0,'C',false);
+		$pdf->Ln(20);
+		$pdf->SetFont('Montserrat-Black','',10);
+		$pdf->Cell(70,5,utf8_decode("FIRMA DEL INTERESADO(A)"),"T",0,'C',false);
+		$pdf->Cell(30,5,"","",0,'C',false);
+		$pdf->Cell(70,5,utf8_decode($pstoesc),"T",0,'C',false);
+		$pdf->Ln();
+		$pdf->SetFont('Montserrat-Black','',10);
+		$pdf->Cell(70,5,utf8_decode(""),"",0,'C',false);
+		$pdf->Cell(30,5,"","",0,'C',false);
+		$pdf->SetFont('Montserrat-Black','',8);
+		$pdf->Cell(70,5,utf8_decode("JEFE(A) DEL DEPARTAMENTO DE SERVICIOS ESCOLARES"),"",0,'C',false);
+		$pdf->Ln(10);
+
+		$pdf->SetFont('Montserrat-Black','',10);
+		$fecha=strtoupper($dataGen[0]["inst_extiende"]). " A ".date("d")." DE ".strtoupper($miutil->getFecha(date("m.d.y"),'MES')). " DEL ".date("Y");
+
+		$pdf->Cell(170,5,utf8_decode($fecha),"",0,'R',false);
+
+
+
+		
 		$pdf->Ln(10);
 
 						
