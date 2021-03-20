1549cec6af85559cba11fd0f388f3b0ae2700f6c
joselupe2002 <joselupe_2002@hotmail.com>
2021-03-20 08:50:19 -0600

    sss

--- nucleo/tit_pasantes/actaExam.php
+++ nucleo/tit_pasantes/actaExam.php
@@ -1,7 +1,7 @@
 
 <?php session_start(); if (($_SESSION['inicio']==1)) {
 	header('Content-Type: text/html; charset='.$_SESSION['encode']);
-	require('../../fpdf/fpdf.php');
+	require('../../fpdf/PDF_WriteTag.php');
 	include("../.././includes/Conexion.php");
 	include("../.././includes/UtilUser.php");
 	$miConex = new Conexion();
@@ -11,7 +11,7 @@
 
 
 	
-   class PDF extends FPDF {
+   class PDF extends PDF_WriteTag {
    	
    	/*========================================================================================================*/
    	var $widths;
@@ -157,11 +157,11 @@
 				$this->SetDrawColor(0,0,0);
 				$this->SetX(20);
 
-				$this->SetY(-40);
+				$this->SetY(-65);
 				$this->Cell(90,4,utf8_decode("COTEJÓ"),'',0,'C',false);
 				$this->Cell(90,4,utf8_decode("DIRECTOR(A) GENERAL"),'',0,'C',false);
 				
-				$this->SetY(-30);
+				$this->SetY(-40);
 				$this->Cell(90,4,utf8_decode($pstoce),'',0,'C',false);
 				$this->Cell(90,4,utf8_decode($pstodg),'',0,'C',false);
 	
@@ -201,18 +201,26 @@
 		$pdf->AddFont('Calibri Bold','','Calibri Bold.php');
 		$pdf->AddFont('Eureka Sans Light','B','Eureka Sans Light.php');
 		$pdf->AddFont('Eureka Sans Light','','Eureka Sans Light.php');
+		$pdf->AddFont('Eureka Sans Light','U','Eureka Sans Light.php');
 
 		$pdf->AddFont('Eureka-Sans-Light-Regular','B','Eureka-Sans-Light-Regular.php');
 		$pdf->AddFont('Eureka-Sans-Light-Regular','','Eureka-Sans-Light-Regular.php');
-		$pdf->Ln(20);
+		$pdf->AddFont('Eureka-Sans-Light-Regular','U','Eureka-Sans-Light-Regular.php');
+		
 
 		$pdf->SetFont('Calibri Bold','',18);
 		$pdf->SetTextColor(12, 30, 97 );
 		$pdf->SetDrawColor(146, 133, 35);
 		$pdf->SetLineWidth(0.7);
-		$pdf->Cell(0,10,utf8_decode($dataGen[0]["inst_razon"]),"B",1,'C');	
+
+		$pdf->Line(54, 42, 154, 42);
+		$pdf->Ln(10);
+		$pdf->Cell(134,5,utf8_decode("Instituto Tecnológico Superior"),"",1,'R');	
 		$pdf->Ln(5);
+		$pdf->Cell(134,5,utf8_decode("de Santa María de El Oro"),"",1,'R');	
 		$pdf->SetTextColor(0);
+
+		$pdf->SetY(60);
 		$pdf->Cell(0,0,utf8_decode("ACTA DE EXÁMEN PROFESIONAL"),0,1,'C');	
 
 		$pdf->Ln(5);
@@ -220,45 +228,77 @@
 		$pdf->SetFont('Eureka-Sans-Light-Regular','',18);
 		$pdf->SetTextColor(0);
 		$pdf->SetFont('Times','',12);
+	
+		/*
 		$pdf->MultiCell(0,7,utf8_decode("El suscrito Director General del ".$dataGen[0]["inst_razon"].", ".
 		"certifica que en el Libro de Actas de Examen Profesional No.".$dataP[0]["LIBRO"]." a las ".$dataP[0]["FOJA"]." fojas, ".
 		"se encuentra asentada el acta  No. ".$dataP[0]["ACTA"]." que a la letra dice:". 
 		" En la ciudad de ".$dataGen[0]["inst_extiende"].", el día ".$eldia." del mes de ".$elmes."  del año ".$elanio.", ".
 		"siendo las ".$dataP[0]["HORA_TITA"]." horas, se reunieron en las instalaciones ".
 		"del ".$dataGen[0]["inst_razon"].", clave ".$dataGen[0]["inst_claveof"]." los integrantes del jurado: "),0,'J',FALSE);
+*/
+
+		$pdf->SetStyle("p","Eureka-Sans-Light-Regular","",10,"0,0,0");
+        $pdf->SetStyle("vs","Eureka-Sans-Light-Regular","U",10,"0,0,0");
+		$pdf->SetStyle("vsb","Eureka-Sans-Light-Regular","UB",10,"0,0,0");
+        $pdf->SetStyle("VS","Eureka-Sans-Light-Regular","U",10,"0,0,0");
+
+		$txt=utf8_decode("<p>El suscrito Director General del ".$dataGen[0]["inst_razon"].", ".
+		"certifica que en el Libro de Actas de Examen Profesional No.<vs>".$dataP[0]["LIBRO"]."</vs> a las <vs>".$dataP[0]["FOJA"]."</vs> fojas, ".
+		"se encuentra asentada el acta  No. <vs>".$dataP[0]["ACTA"]."</vs> que a la letra dice:</p>");
+        $pdf->WriteTag(0,6,$txt,0,"J",0,0);
+
+		$pdf->Ln();
+
+		$txt2=utf8_decode("<p> En la ciudad de ".$dataGen[0]["inst_extiende"].", el día <vs>".$eldia."</vs> del mes de <vs>".$elmes."</vs>  del año <vs>".$elanio."</vs>, ".
+		"siendo las <vs>".$dataP[0]["HORA_TITA"]."</vs> horas, se reunieron en las instalaciones".
+		"del ".$dataGen[0]["inst_razon"].", clave ".$dataGen[0]["inst_claveof"]." los integrantes del jurado: </p>");
+		$pdf->WriteTag(0,6,$txt2,0,"J",0,0);
+
+
+		$pdf->SetDrawColor(0);
+		$pdf->SetLineWidth(0.2);
 
 		$pdf->Ln(7);
 		$pdf->Cell(30,7,utf8_decode("Presidente(a):"),0,0,'L');	
-		$pdf->Cell(120,7,utf8_decode($dataP[0]["PRESAD"]),0,0,'L');	
+		$pdf->Cell(120,7,utf8_decode($dataP[0]["PRESAD"]),"B",0,'L');	
 
 		$pdf->Ln(7);
 		$pdf->Cell(30,7,utf8_decode("Secretario(a):"),0,0,'L');	
-		$pdf->Cell(120,7,utf8_decode($dataP[0]["SECAD"]),0,0,'L');	
+		$pdf->Cell(120,7,utf8_decode($dataP[0]["SECAD"]),"B",0,'L');	
 
 		$pdf->Ln(7);
 		$pdf->Cell(30,7,utf8_decode("Vocal:"),0,0,'L');	
-		$pdf->Cell(120,7,utf8_decode($dataP[0]["VOCAD"]),0,0,'L');	
+		$pdf->Cell(120,7,utf8_decode($dataP[0]["VOCAD"]),"B",0,'L');	
 
 		$pdf->Ln(10);
 
-		$pdf->MultiCell(0,7,utf8_decode("Y de acuerdo con las disposiciones reglamentarias en vigor y la opción seleccionada: ".
-		$dataP[0]["OPCIOND"].", se procedió a llevar a cabo el Acto de Recepción Profesional a ".$dataP[0]["PASANTE"].
-		", con número de control ".$dataP[0]["MATRICULA"].", pasante de la carrera de: ".$dataP[0]["CARRERAD"].
-		" con clave del plan de estudios ".$dataP[0]["MAPA"]."."),0,'J',FALSE);
 
+		$txt=utf8_decode("<p>Y de acuerdo con las disposiciones reglamentarias en vigor y la opción seleccionada: <vsb>".
+		$dataP[0]["OPCIOND"]."</vsb>, se procedió a llevar a cabo el Acto de Recepción Profesional a <vs>".$dataP[0]["PASANTE"].
+		"</vs>, con número de control <vs>".$dataP[0]["MATRICULA"]."</vs>, pasante de la carrera de: <vs>".$dataP[0]["CARRERAD"].
+		"</vs> con clave del plan de estudios <vs>".$dataP[0]["MAPA"]."</vs>.</p>");
+		$pdf->WriteTag(0,6,$txt,0,"J",0,0);
 
+		$pdf->Ln();
 
-		$pdf->MultiCell(0,7,utf8_decode("Tomando en cuenta los integrantes del jurado las facultades mostradas en el acto de ".
-		"recepción, se dictaminó que fuera: ".$dataP[0]["RESULTADO"]."."),0,'J',FALSE);
+		$txt=utf8_decode("<p>Tomando en cuenta los integrantes del jurado las facultades mostradas en el acto de ".
+		"recepción, se dictaminó que fuera: <vs>".$dataP[0]["RESULTADO"]."</vs>.</p>");
+		$pdf->WriteTag(0,6,$txt,0,"J",0,0);
 
+		$pdf->Ln();
 
+		$txt=utf8_decode("<p>El(la) Presidente(a) del Jurado hizo saber al sustentante el resultado obtenido, ".
+		"el Código de Ética Profesional y le tomó la Protesta de Ley.  Dándose por terminado el Acto a las <vs>".$dataP[0]["HORA_TITAT"].
+		"</vs> horas, y una vez escrita, leída y aprobada la firmaron para constancia las personas que el acto intervinieron.</p>");
+		$pdf->WriteTag(0,6,$txt,0,"J",0,0);
 
-		$pdf->MultiCell(0,7,utf8_decode("El(la) Presidente(a) del Jurado hizo saber al sustentante el resultado obtenido, ".
-		"el Código de Ética Profesional y le tomó la Protesta de Ley.  Dándose por terminado el Acto a las ".$dataP[0]["HORA_TITAT"].
-		" horas, y una vez escrita, leída y aprobada la firmaron para constancia las personas que el acto intervinieron."),0,'J',FALSE);
+		$pdf->Ln();
 
-		$pdf->MultiCell(0,7,utf8_decode("Para los usos legales correspondientes se expide la presente en la ciudad de ".$dataGen[0]["inst_extiende"].
-		", a los ".$eldiaA." días del mes de ".$elmesA." del año ".$elanioA."."),0,'J',FALSE);
+		$txt=utf8_decode("<p>Para los usos legales correspondientes se expide la presente en la ciudad de Santa María del Oro, Durango,".
+		" a los <vs>".$eldiaA."</vs> días del mes de <vs>".$elmesA."</vs> del año <vs>".$elanioA."</vs>.</p>");
+		$pdf->WriteTag(0,6,$txt,0,"J",0,0);
+
 			
 		$pdf->Output();
 
