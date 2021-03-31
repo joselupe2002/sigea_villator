ea0eb57b5830e9ab118120866ea8ace914ab0879
joselupe2002 <joselupe_2002@hotmail.com>
2021-03-31 14:23:16 -0600

    sss

--- admision/ex.js
+++ admision/ex.js
@@ -740,7 +740,7 @@ function cargarAdjuntos() {
 					"            <input class=\"fileSigea\" type=\"file\" id=\"file_"+valor.CLAVE+"\""+
 					"                   onchange=\"subirPDFDriveSaveAsp_local('file_"+valor.CLAVE+"','docAspira','pdf_"+
 												  valor.CLAVE+"','RUTA_"+valor.CLAVE+"','"+valor.TIPOADJ+"','S','ID','"+valor.CLAVE+
-												  "',' DOCUMENTO  "+valor.DOCUMENTO+" ','adjaspirantes','alta','"+usuario+"_"+elciclo+"','"+valor.CLAVE+"_"+usuario+"_"+elciclo+"');\">"+
+												  "',' DOCUMENTO  "+valor.DOCUMENTO+" ','adjaspirantes','alta','"+valor.CLAVE+"_"+usuario+"_"+elciclo+"','"+valor.CLAVE+"_"+usuario+"_"+elciclo+"');\">"+
 					"           <input  type=\"hidden\" value=\""+valor.RUTA+"\"  name=\"RUTA_"+valor.CLAVE+"\" id=\"RUTA_"+valor.CLAVE+"\"  placeholder=\"\" />"+
 					"        </div>"+
 					"        <div class=\"col-sm-1\" style=\"padding-top:5px;\">"+
