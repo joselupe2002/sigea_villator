ebc452fb412e01d51353bd940474f762585d36bf
joselupe2002 <joselupe_2002@hotmail.com>
2021-04-19 15:52:00 -0500

    ddd

--- nucleo/vejustifica/proc_vejustifica.js
+++ nucleo/vejustifica/proc_vejustifica.js
@@ -2,7 +2,7 @@
 
 function impJustifica(modulo,usuario,institucion, campus,essuper){
 	table = $("#G_"+modulo).DataTable();
-	enlace="nucleo/vejustifica/justificante.php?id="+table.rows('.selected').data()[0][0]+"&tipo=0":
+	enlace="nucleo/vejustifica/justificante.php?id="+table.rows('.selected').data()[0][0]+"&tipo=0";
 	abrirPesta(enlace,"Justificante" );
 
     return false;
