a3a3f0e309516db2b64896b28c4c019e02ec661a
joselupe2002 <joselupe_2002@hotmail.com>
2021-11-16 11:43:54 -0600

    SSS

--- nucleo/pd_captcal/evaluar.php
+++ nucleo/pd_captcal/evaluar.php
@@ -302,6 +302,10 @@
 			if (eltidepocorte=='CCO1'){tipocal="1";}
 			if (eltidepocorte=='CCO2'){tipocal="1";}
 			if (eltidepocorte=='CCO3'){tipocal="1";}
+			if (eltidepocorte=='CCO4'){tipocal="1";}
+			if (eltidepocorte=='CCO5'){tipocal="1";}
+			if (eltidepocorte=='CCO6'){tipocal="1";}
+
 			if (eltidepocorte=='CCC1'){tipocal="2";}
 			if (eltidepocorte=='CCC2'){tipocal="3";}
 
